9bf4c0f127f08c6adb65b5f4809e6393d988c4cb
Ortal Maslati <ortalmaslati@gmail.com>
2019-10-27 17:46:47 +0200

    add ecco service
    Note: need to run VS as administrator!

--- E-Elections-Server/E-Elections-Server/Program.cs
+++ E-Elections-Server/E-Elections-Server/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
 
 namespace E_Elections_Server
 {
@@ -10,7 +13,13 @@ namespace E_Elections_Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            WebServiceHost hostWeb = new WebServiceHost(typeof(E_Elections_Server.Service));
+            ServiceEndpoint ep = hostWeb.AddServiceEndpoint(typeof(E_Elections_Server.IService), new WebHttpBinding(), "");
+            ServiceDebugBehavior stp = hostWeb.Description.Behaviors.Find<ServiceDebugBehavior>();
+            stp.HttpHelpPageEnabled = false;
+            hostWeb.Open();
+            Console.WriteLine("Service Host started @ " + DateTime.Now.ToString());
+            Console.Read();
         }
     }
 }
